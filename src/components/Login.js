3c64b9dc34e5f0d604348b4b497db7d0e9a25c86
Shekhar Sajwan <shekharsajwan97@gmail.com>
2024-09-22 14:33:47 +0530

    Router and homepage created

---
+++ src/components/Login.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div>
+        <img
+          className="w-full size-full absolute"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/47c2bc92-5a2a-4f33-8f91-4314e9e62ef1/web/IN-en-20240916-TRIFECTA-perspective_72df5d07-cf3f-4530-9afd-8f1d92d7f1a8_medium.jpg"
+        />
+      </div>
+      <form className="w-1/3 mx-auto absolute left-0 my-36 right-0 p-8 bg-gradient-to-tr from-red-50 to-transparent">
+        <h1 className="font-bold text-3xl mb-3">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email or phone number"
+          className="w-full bg-gray-300 p-1 mb-4 rounded border-2 border-gray-500"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full bg-gray-300 p-1 mb-4 rounded border-2 border-gray-500"
+        />
+        <button className="w-full px-2 py-1 text-sm rounded-sm bg-red-600 text-white">
+          Submit
+        </button>
+        <p className="mt-1 font-bold ">
+          New to Netflix?{" "}
+          <a className="text-red-600" href="#">
+            Sign Up Now
+          </a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
