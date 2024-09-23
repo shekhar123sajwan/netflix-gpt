184d4be6f77e629fd0ef2dd1b9243204fcec241b
Shekhar Sajwan <shekharsajwan97@gmail.com>
2024-09-23 10:44:10 +0530

    movies api call created

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
+import { API_OPTION } from "../utils/constants";
 
 const Browse = () => {
+  const [movies, setMovies] = useState([]);
+  const getNowPlayingMovies = async () => {
+    const url =
+      "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+
+    const movieList = await fetch(url, API_OPTION);
+
+    const { results } = await movieList.json();
+  };
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
   return <Header />;
 };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,12 @@
 export const PHOTO_URL = "https://avatars.githubusercontent.com/u/34606343?v=4";
 export const LOGO =
   "https://images.ctfassets.net/y2ske730sjqp/6bhPChRFLRxc17sR8jgKbe/6fa1c6e6f37acdc97ff635cf16ba6fb3/Logos-Readability-Netflix-logo.png";
+
+export const API_OPTION = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTMwOTA3YTJjZTI5ZjMyMTMxZWI2MjhhYzI0OTYyNSIsIm5iZiI6MTcyNzAyNDQwNy44MjA5NDMsInN1YiI6IjYzOTRjMDk5ZDA1YTAzMDBjZjI5OTg3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v_KHvpXZ7dxVgIfKgtxcyF_aCTSV8Nx-Eg1kt5VqLDs",
+  },
+};
